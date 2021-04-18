a4dae4fa4fa849d32bbd4406a0ff9b708d56eeb3
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-04-18 17:47:50 +0300

    New Pages uploaded

---
+++ clients/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', function () {
+    let home = document.querySelector('.home-link');
+    let domain = document.querySelector('.domain-link');
+    let hosting = document.querySelector('.hosting-link');
+    let clients = document.querySelector('.clients-link');
+    let blog = document.querySelector('.blog-link');
+    let support = document.querySelector('.support-link');
+
+    home.addEventListener('click', openHome);
+    domain.addEventListener('click', openDomain);
+    hosting.addEventListener('click', openHosting);
+    clients.addEventListener('click', openClients);
+    blog.addEventListener('click', openBlog);
+    support.addEventListener('click', openSupport);
+
+    function openHome() {
+        window.location.href = '../index.html';
+    }
+
+    function openDomain() {
+        window.location.href = '../domain/index.html';
+    }
+
+    function openHosting() {
+        window.location.href = '../hosting/index.html';
+    }
+
+    function openClients() {
+        window.location.href = './index.html';
+    }
+
+    function openBlog() {
+        window.location.href = '../blog/index.html';
+    }
+
+    function openSupport() {
+        window.location.href = '../support/index.html';
+    }
+})
